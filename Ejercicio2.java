1cca6e789224d81bf1049d8b60bbd22b02bdb3a7
Dafer3013 <106795516+Dafer3013@users.noreply.github.com>
2022-06-02 22:40:38 -0600

    Add files via upload

---
+++ Ejercicio2.java
@@ -0,0 +1,14 @@
+/* Ejercicio 2.22*/
+
+
+public class Ejercicio2{
+    
+
+    public static void main(String[]arg){
+        System.out.print("*");
+        System.out.println("***");
+        System.out.println("*****");
+        System.out.print("****");
+        System.out.println("**");
+    }
+}
---
+++ Ejercicio4.java
@@ -0,0 +1,10 @@
+/* Ejercicio 2.19*/
+
+
+public class Ejercicio4{
+    
+
+    public static void main(String[]arg){
+        System.out.printf("*%n**%n***%n****%n*****%n");
+    }
+}
